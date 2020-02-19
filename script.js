b4440b2394e1259b201a0ec4358fc176d1a2c30b
Dan33311 <you@example.com>
2020-02-19 16:31:46 -0500

    added script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// ***CATEGORIA 1***   FECHA FIJA
+// No necesita calculo
+// Año Nuevo -------------------------------- 1 de enero
+// Día del trabajo -------------------------- 1 de mayo
+// Día de la independencia ----------------- 20 de julio
+// Batalla de Boyacá ------------------------ 7 de agosto
+// Inmaculada concepción -------------------- 8 de diciembre
+// Navidad --------------------------------- 25 de diciembre
+
+
+// ***CATEGORIA 2***   SEGUN PASCUA
+// Se calculan sumando o restando días respecto a la fecha de celebración del Domingo de Pascua
+// Jueves Santo ----------------------------- pascua  -3 dias
+// Viernes Santo ---------------------------- pascua  -2 dias
+// Ascención de Jesús ----------------------- pascua +43 dias
+// Corpus Christi  -------------------------- pascua +64 dias
+// Sagrado corazón de Jesús ----------------- pascua +71 dias
+
+
+// ***CATEGORIA 3***   FECHA TRASLADABLE     
+// si la fecha base no es lunes, se traslada al siguiente lunes
+// Epifanía del señor (reyes magos)---------- 6 de enero
+// Día de San José ------------------------- 19 de marzo
+// San Pedro y San Pablo ------------------- 29 de junio
+// Jueves Santo
+// Viernes Santo
+// Asunción de la Virgen ------------------- 15 de agosto
+// Día de la raza -------------------------- 12 de octubre
+// Todos los santos ------------------------- 1 de noviembre
+// Independencia de Cartagena -------------- 11 de noviembre
