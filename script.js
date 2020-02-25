4e2979dd31afac5ad42ea0601a3a726ee6b4b537
Dan33311 <you@example.com>
2020-02-25 16:01:55 -0500

    cambios en js

--- script.js
+++ script.js
@@ -8,7 +8,9 @@
 // Navidad --------------------------------- 25 de diciembre
 
 
-// ***CATEGORIA 2***   SEGUN PASCUA
+// ***CATEGORIA 2***   SEGUN PASCUA      domingo inmediatamente posterior a la primera luna llena 
+                                        //después del equinoccio de marzo
+                                        //no antes del 22 de marzo y el 25 de abril como muy tarde.
 // Se calculan sumando o restando días respecto a la fecha de celebración del Domingo de Pascua
 // Jueves Santo ----------------------------- pascua  -3 dias
 // Viernes Santo ---------------------------- pascua  -2 dias
@@ -28,3 +30,67 @@
 // Día de la raza -------------------------- 12 de octubre
 // Todos los santos ------------------------- 1 de noviembre
 // Independencia de Cartagena -------------- 11 de noviembre
+
+
+var year = "2020" // año a mostrar el calendario // caso de uso: 2020 o mayor
+var equinoccio = new Date("03/19/2020");
+var fullmoon = new Date("01/10/2020"); // acumalado .5
+var accum = true
+
+
+
+var getEaster = function(year, equinoccio){
+    tmpFullMoon = fullmoon;
+    a = 0
+    while(tmpFullMoon<equinoccio && a<9){
+        tmpFullMoon.setDate(tmpFullMoon.getDate()+(accum?30:29))
+        accum = !accum
+            
+        //console.log("aaaa")
+        a++
+    }
+    //ahora calcular domingo pascua
+    return tmpFullMoon
+}     
+var fullMoonEaster = getEaster(year, equinoccio);
+console.log(fullMoonEaster)
+
+
+
+function setToSunday(fullMoonEaster){
+    var day = fullMoonEaster.getDay() || 7;  
+    if(day !== 7) 
+    fullMoonEaster.setHours(-24 * (day - 7)); 
+    return fullMoonEaster;
+}
+var sundayEaster = setToSunday(fullMoonEaster);
+console.log(sundayEaster,"rtyu")//domingo de pascua
+
+
+
+var holyThursday = new Date(sundayEaster)
+holyThursday.setDate(holyThursday.getDate()-(3));
+console.log(new Date(holyThursday)) //jueves santo
+
+var goodFriday = new Date(sundayEaster);
+goodFriday.setDate(goodFriday.getDate()-(2));
+console.log(new Date(goodFriday))//viernes santo
+
+
+var memorialDay = new Date(sundayEaster);
+memorialDay.setDate(memorialDay.getDate()+(43));
+console.log(new Date (memorialDay))//Ascension de jesus
+
+var corpusChristi = new Date(sundayEaster);
+corpusChristi.setDate(corpusChristi.getDate()+(64));
+console.log(new Date (corpusChristi))//corpus Chisti
+
+var SacredHeartofJesus = new Date(sundayEaster);
+SacredHeartofJesus.setDate (SacredHeartofJesus.getDate()+(71));
+console.log(new Date(SacredHeartofJesus))//sagrado corazon de Jesus
+
+
+
+
+// Viernes santo = GoodFriday
+//var daysWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
